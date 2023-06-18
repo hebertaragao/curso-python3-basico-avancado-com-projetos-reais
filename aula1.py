3e2f5d0ace2d9f46c472843c5b7ddfffe357c699
hebertaragao <hebert.aragao@outlook.com>
2023-06-18 17:31:58 -0300

    DocString  e comentarios

--- aula1.py
+++ aula1.py
@@ -1,2 +1,15 @@
+"""
+DocString não é comentário,
+ porém alguns desenvolvedores 
+ usam como comentário de várias linhas
+"""
+
+'''
+DocString pode ser usado com aspas
+simples ou duplas
+'''
+
 # Permite escrever um comentário
 print(123) # Na frente
+print(456)
+
