8c26dced3825a5a0f42140d550d46973408bfc16
hebertaragao <hebert.aragao@outlook.com>
2023-06-29 21:05:40 -0300

    aula_013.py

---
+++ aula_013.py
@@ -0,0 +1,11 @@
+nome = 'Hebert Aragão'
+altura = 1.75
+peso = 82
+imc = peso / (altura * altura)
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é, '
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
