8e85f33550f9d9911e5f7018a7a7bdbe2d75dfd7
hebertaragao <hebert.aragao@outlook.com>
2023-10-10 20:21:19 -0300

    aula_031.py

--- aula_031.py
+++ aula_031.py
@@ -4,14 +4,19 @@ None = Não valor
 is e is not = é ou não é (tipo, valor, identidade)
 id = Identidade
 """
-v1 = 'a'
-v2 = 'b'
-print(id(v1))
-print(id(v2))
+condicao = True
+passou_no_if = None
 
-# condicao = False
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
 
-# if condicao:
-#   print('Faça algo')
-# else:
-#   print('Não faça algo')
+print(passou_no_if, passou_no_if is None)
+print(passou_no_if, passou_no_if is not None)
+
+if passou_no_if is None:
+    print('Não passou no if')
+else:
+    print('Passou no if')
