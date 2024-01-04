3b0e7ce931e7651f0c2d40d482d161fcb02c14d1
hebertaragao <hebert.aragao@outlook.com>
2024-01-04 17:05:03 -0300

    aula_039.py

---
+++ secao_3/aula_039.py
@@ -0,0 +1,19 @@
+"""
+Iterando strings com while
+"""
+
+#       012345678910
+
+nome = "Hebert Aragão" #Iteráveis
+#       1110987654321
+
+indice = 0
+novo_nome = ''
+
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += f'*{letra}'
+    indice += 1
+
+novo_nome += "*"
+print(novo_nome)
