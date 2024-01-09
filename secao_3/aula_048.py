1342cf7a8b8bd028e580e44b3e4c927b63864138
hebertaragao <hebert.aragao@outlook.com>
2024-01-09 14:47:37 -0300

    aula_048.py

---
+++ secao_3/aula_048.py
@@ -0,0 +1,17 @@
+"""
+Listas em python
+Tipo list Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+#       01234
+#       54321
+string = 'ABCDE' # 5 caracteres
+lista = [123, True, 'Hebert Aragão', 1.2, []]
+# print(bool([])) # false
+#print(lista, type(lista))
+lista[-3] = 'Maria'
+print(lista)
+print(lista[2], type(lista[2]))
